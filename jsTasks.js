462835f49e41a912098edfcc423684f095394cf5
Zicozydasilva <Zicozydasilva70@gmail.com>
2020-03-23 21:09:37 +0100

    first commit

---
+++ jsTasks.js
@@ -0,0 +1,14 @@
+//store name in varable
+// store course in an array
+//Set conditionals:
+// if numOfCourse = odd, find and display all odd numbers that're btw 1-200 inclusive
+//if numOfCourse = even, find and display all even numbers from 1-200 inclusive.
+
+const name = `Ogbu Ezekiel Ike`;
+const courses = [`HTML5`, `CSS3`, `Javascript`];
+
+ for(i = 1; i <= 200; i++) {
+   if(courses % 2 !== 0) {
+     console.log(i)
+   }
+ }
